130843c4fac7472fbf6b2b146d0a831f51be152d
vikkykumar <vikkykumar@192.168.0.4>
2019-09-02 18:41:04 +0530

    Commit First

--- HelloRest/src/main/java/com/example/demo/HelloRestApplication.java
+++ HelloRest/src/main/java/com/example/demo/HelloRestApplication.java
@@ -3,11 +3,7 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
-import com.example.demo.aop.dao.AopDao;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
@@ -17,10 +13,6 @@ public class HelloRestApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(HelloRestApplication.class, args);
-		//@Autowired
-		//AopDao aopDao = new AopDao();
-		//aopDao.printData();
-		//HelloRestApplication helloRestApplication = new HelloRestApplication();
 		
 		
 	}
