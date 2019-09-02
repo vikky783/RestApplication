0f9fccc1a61c2c1aa9525ee407947b43ae9800ca
vikkykumar <vikkykumar@192.168.0.4>
2019-09-02 23:19:13 +0530

    Response Entity Detailed value

--- HelloRest/src/main/java/com/example/demo/user/UserController.java
+++ HelloRest/src/main/java/com/example/demo/user/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -27,6 +28,7 @@ public class UserController {
 	}
 	
 	@GetMapping("/aUser/{empId}")
+	@ResponseBody
 	public User getOneUser(@PathVariable int empId) {
 		int empId1 = empId;
 		return userDaoServices.getOneUser( empId1);
@@ -45,7 +47,7 @@ public class UserController {
 				.path("/{id}")
 				.buildAndExpand(savedUser.getEmpId()).toUri();
 		
-		return ResponseEntity.created(location).build();
+		return ResponseEntity.created(location).header("My First Header", "Hello How are you").body("Hello Brother");
 		
 	}
 	
